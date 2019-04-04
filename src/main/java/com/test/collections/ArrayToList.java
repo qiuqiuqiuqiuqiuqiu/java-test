73d0b613c271c0bcee5f597df085eea465c69c0c
ethan.qiu <ethan.qiu@oracle.com>
2019-04-04 10:21:38 +0800

    test

--- src/main/java/com/test/collections/ArrayToList.java
+++ src/main/java/com/test/collections/ArrayToList.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ArrayToList {
-
+    
     public static void main(String[] args) {
         String[] vowels = {"a", "e", "i", "o", "u"};
 
